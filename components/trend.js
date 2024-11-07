42ddafc9436d968d291070cf82790cf805ff2eef
Murilo <mbalves1@outlook.com>
2024-11-07 16:21:05 -0300

    [feat]: "Updated Trend component to display percentage change and added Lucide React icons; updated dependencies in package.json"

--- components/trend.js
+++ components/trend.js
@@ -1,3 +1,6 @@
+import { useMemo } from "react";
+import { ArrowDownLeft, ArrowUpRight } from "lucide-react";
+
 export default function Trend({
   type, amount, prevAmount
 }) {
@@ -7,15 +10,20 @@ export default function Trend({
     "Expense": "text-red-700 dark:text-red-300",
     "Investment": "text-indigo-700 dark:text-indigo-300",
     "Saving": "text-yellow-700 dark:text-yellow-300",
-  }
+  };
 
   const calcPercentageChange = (amount, prevAmount) => {
-    if (prevAmount === 0) return 0;
+    if (!prevAmount  || !amount) return 0;
     return ((amount - prevAmount) / prevAmount) * 100;
-  }
+  };
+
+  const percentageChange = useMemo(
+    () => calcPercentageChange(amount, prevAmount).toFixed(0),
+    [amount, prevAmount]
+  );
 
   const formatCurrency = (amount) =>
-    new Intl.NumberFormat('en-US', {style: 'currency', currency: 'EUR'}).format(amount)
+    new Intl.NumberFormat('en-US', {style: 'currency', currency: 'EUR'}).format(amount);
 
   return (
     <div>
@@ -23,6 +31,11 @@ export default function Trend({
 
       <div className="text-2xl font-semibold text-black dark:text-white mb-2">
         { amount ? formatCurrency(amount) : formatCurrency(0) }
+      </div>
+      <div className="flex space-x-1 items-center text-sm">
+        { percentageChange <= 0 && <ArrowDownLeft className="text-red-700 dark:text-red-300" /> }
+        { percentageChange > 0 && <ArrowUpRight className="text-green-700 dark:text-green-300" /> }
+        <div>{ percentageChange }% vs last period</div>
       </div>
     </div>
   )
