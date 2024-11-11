0c9d6ab6a2c81468f02d5c458b0ff8c765fe9f33
Murilo <mbalves1@outlook.com>
2024-11-11 08:45:29 -0300

    [refactor]: "Extracted currency formatting into a hook and replaced inline formatting with hook call"

---
+++ hooks/use-format-currency.js
@@ -0,0 +1,12 @@
+import { useMemo } from "react";
+
+export const useFormatCurrency = (amount) => {
+  const formatCurrency = (amount) => {
+    return new Intl.NumberFormat('en-US', {style: 'currency', currency: 'EUR'}).format(amount);
+  }
+
+  return useMemo(
+    () => formatCurrency(amount),
+    [amount]
+  )
+}
