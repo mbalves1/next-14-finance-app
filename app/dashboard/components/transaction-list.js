47cb95a87759bcbdb5ba7cbce288883db926d3ba
Murilo <mbalves1@outlook.com>
2024-11-22 10:24:33 -0300

    [feat]: "Update layout and page components with new footer and transaction list"

---
+++ app/dashboard/components/transaction-list.js
@@ -0,0 +1,21 @@
+import TransactionItem from "@/components/transaction-item";
+
+export default async function TransactionList() {
+  const response = await fetch(
+    'http://localhost:3100/transactions'
+  );
+
+  const transactions = await response.json();
+  console.log('transactions', transactions);
+
+  return (<section className="space-y-4">
+    { transactions.map(transaction => <div key={transaction.id}>
+      <TransactionItem
+        type={transaction.type}
+        category={transaction.category}
+        description={transaction.description}
+        amount={transaction.amount}
+      ></TransactionItem>
+    </div>) }
+  </section>);
+}
--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={theme}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
         {children}
       </body>
