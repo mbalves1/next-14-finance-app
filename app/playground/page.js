07d52c813e3f17dd8f012168c705040ad2a57e3a
Murilo <mbalves1@outlook.com>
2024-11-25 10:01:52 -0300

    [refactor]: "Replace hardcoded hr elements with Separator component imports"

--- app/playground/page.js
+++ app/playground/page.js
@@ -3,6 +3,8 @@ import Input from "@/components/input";
 import Label from "@/components/label";
 import PageHeader from "@/components/page-header";
 import Select from "@/components/select";
+import Separator from "@/components/separator";
+import Skeleton from "@/components/skeleton";
 import TransactionItem from "@/components/transaction-item";
 import TransactionSummaryItem from "@/components/transaction-summary-item";
 import Trend from "@/components/trend";
@@ -14,7 +16,7 @@ export default function Page() {
 
       <div>
         <h2 className="mb-4 text-lg font-mono">PageHeader</h2>
-        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <Separator />
         <div>
           <PageHeader></PageHeader>
         </div>
@@ -22,7 +24,7 @@ export default function Page() {
 
       <div>
         <h2 className="mb-4 text-lg font-mono">Trend</h2>
-        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <Separator />
         <div className="flex space-x-8">
           <Trend type="Income" amount={1000} prevAmount={900} />
           <Trend type="Expense" amount={12000} prevAmount={10000} />
@@ -33,7 +35,7 @@ export default function Page() {
 
       <div>
         <h2 className="mb-4 text-lg font-mono">TransactionItem</h2>
-        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <Separator />
         <div className="space-y-4">
           <TransactionItem type="Income" description="Salary" amount={2000} />
           <TransactionItem type="Expense" description="Going out to eat" category="food" amount={29} />
@@ -44,10 +46,10 @@ export default function Page() {
 
       <div>
         <h2 className="mb-4 text-lg font-mono">TransactionSummaryItem + TransactionItem</h2>
-        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <Separator />
         <div className="space-y-4">
           <TransactionSummaryItem date="2024-05-01" amount={3500} />
-          <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+          <Separator />
 
           <TransactionItem type="Income" description="Salary" amount={2000} />
           <TransactionItem type="Expense" description="Going out to eat" category="food" amount={29} />
@@ -58,7 +60,7 @@ export default function Page() {
 
       <div>
         <h2 className="mb-4 text-lg font-mono">Buttons</h2>
-        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <Separator />
         <div className="space-x-4">
           <Button>Hello</Button>
           <Button variant="outline">Hello</Button>
@@ -72,7 +74,7 @@ export default function Page() {
 
       <div>
         <h2 className="mb-4 text-lg font-mono">Forms</h2>
-        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <Separator />
         <div className="grid grid-cols-2 gap-4">
           <div>
             <Label className="mb-1">Your name</Label>
@@ -91,6 +93,24 @@ export default function Page() {
           <div className="flex items-center">
             <Input type="checkbox"/>
             <Label className="ml-2" htmlFor="terms">Accept terms</Label>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">Loading Skeleton</h2>
+        <Separator />
+        <div className="space-y-8">
+          <div className="flex space-x-4">
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+          </div>
+
+          <div className="space-y-4">
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
           </div>
         </div>
       </div>
