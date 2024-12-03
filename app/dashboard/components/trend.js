a9e13c90a893b52b4186b0817fb6ff9517871d84
Murilo <mbalves1@outlook.com>
2024-12-03 18:15:29 -0300

    [refactor]: "Refactor Trend component to use Supabase RPC and update transactionSchema validation rules"

--- app/dashboard/components/trend.js
+++ app/dashboard/components/trend.js
@@ -1,14 +1,18 @@
 import BaseTrend from "@/components/trend";
+import { createClient } from "@/lib/supabase/server";
 
 export default async function Trend({ type }) {
-  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  const supabase = await createClient();
 
-  const response = await fetch(`${process.env.API_URL}/trends/${type}`);
+  let { data, error } = await supabase
+    .rpc('calculate_total', {
+      type_arg: type
+    });
+  if (error) throw new Error("Could not fetch the trend data");
 
-  await delay(3000);
-  const { amount, prevAmount } = await response.json();
+  const amount = data ?? 0;
 
   return (
-    <BaseTrend type={type} amount={amount} prevAmount={prevAmount}  />
+    <BaseTrend type={type} amount={amount} prevAmount={amount - 500}  />
   )
 }
