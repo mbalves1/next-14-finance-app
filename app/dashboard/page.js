719f38815bbb8b9635cd0f19a0252039ec1551a7
Murilo <mbalves1@outlook.com>
2024-12-05 13:10:26 -0300

    [feat]: "Update range component to use value instead of defaultValue, refactor TransactionList to accept range prop and use rpc to fetch transactions"

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -38,7 +38,7 @@ export default async function Page({ searchParams }) {
       </section>
 
       <Suspense fallback={<TransactionListFallback />}>
-        <TransactionList />
+        <TransactionList range={range} />
       </Suspense>
     </>
   )
