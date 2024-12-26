7e60b6f67880713e224004b5b88a16091c8de971
Murilo <mbalves1@outlook.com>
2024-12-26 13:43:12 -0300

    `[refactor]: alert components and avatar upload logic`

---
+++ components/alert-error.js
@@ -0,0 +1,10 @@
+import { Ban } from "lucide-react";
+import Alert from "./alert";
+
+export default function AlertSuccess({children}) {
+  return (<>
+    <Alert icon={<Ban className="text-red-700 dark:text-red-300 w-6 h-6"/>} title={<span className="text-red-700 dark:text-red-300">Error</span>}>
+      <span className="text-red-700 dark:text-red-300">{children}</span>
+    </Alert>
+  </>)
+}
